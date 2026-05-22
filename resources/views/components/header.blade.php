ca6d5bd9e29d797df970836b1f2512e198747bbf
Sabin Kasereka <salambungusabin@gmail.com>
2026-05-22 07:58:03 +0100

    Dernier ajustement !

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -19,7 +19,7 @@
         <ul class="nav-links">
             {{-- Utilisation de la fonction route() avec le nom de vos routes dans web.php --}}
             <li><a href="{{ route('home') }}">Accueil</a></li>
-            <li><a href="{{ route('articles.index') }}">Articles</a></li>
+            <li><a href="{{ route('articles.index') }}/Sabin">Articles</a></li>
             <li><a href="{{ route('categories.index') }}">Catégories</a></li>
             <li><a href="{{ route('about') }}" class="active">À propos</a></li>
             
